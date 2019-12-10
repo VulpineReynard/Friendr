fecbd35edf74ae81e82f798b39410a8d4209d52c
VulpineReynard <milare8@gmail.com>
2019-12-09 19:01:14 -0500

    server-side validation for user signup

--- functions/index.js
+++ functions/index.js
@@ -71,6 +71,17 @@ app.post('/scream', (req, res) => {
     })
 })
 
+const isEmail = string => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (string.match(regEx)) return true;
+  else return false;
+}
+
+const isEmpty = string => {
+  if (string.trim() === '') return true;
+  else return false;
+}
+
 // Signup Route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -79,6 +90,18 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   }
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) errors.email = "Must not be empty.";
+  else if (!isEmail(newUser.email)) errors.email = "Must be a valid email address.";
+
+  if (isEmpty(newUser.password)) errors.password = "Must not be empty.";
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = "Passwords must match.";
+
+  if (isEmpty(newUser.handle)) errors.handle = "Must not be empty.";
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
   //TODO: Validate data
   let token, userId;
